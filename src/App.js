be6627b63da0c66a2d231d3e635c073c3a8e5157
amr elgendy <amr86tawfik@gmail.com>
2021-06-07 22:44:42 +0200

    test two

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import img from './images/cover.png';
 import data from './data';
-// import aud from './images/mixkit-fast-small-sweep-transition-166.wav'
 
 function App() {
   const [isFlip, setIsFlip] = useState(null);
@@ -15,14 +13,12 @@ function App() {
           className={isFlip === i ? 'card flip' : 'card'}
           onClick={() => setIsFlip(i)}
         >
-          <div className=''></div>
           <div className='face front'>
-            <div className="number"></div>
-            <img src={img} alt="camera" />
+            <img src={p.image} alt="cover" />
           </div>
           <div className='face back'>
-            <p className='text'>{p.text}</p>
-            <p>From : {p.from}</p>
+            {/* <p className='text'>{p.text}</p> */}
+            {/* <p>From : {p.from}</p> */}
             <audio src={p.src} controls></audio>
           </div>
         </div>
