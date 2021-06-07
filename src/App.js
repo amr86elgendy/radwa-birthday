4893d30ef71ba9cc1c54a58950b3e89ef82205eb
amr elgendy <amr86tawfik@gmail.com>
2021-06-07 21:50:40 +0200

    test one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import img from './images/cover.png';
+import data from './data';
+// import aud from './images/mixkit-fast-small-sweep-transition-166.wav'
 
 function App() {
+  const [isFlip, setIsFlip] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <h1 className='title'>Happy Birthday DR/ Radwa-Elmoneir</h1>
+    <div className='container'>
+      {data.map((p, i) => (
+        <div
+          key={i}
+          className={isFlip === i ? 'card flip' : 'card'}
+          onClick={() => setIsFlip(i)}
         >
-          Learn React
-        </a>
-      </header>
+          <div className=''></div>
+          <div className='face front'>
+            <div className="number"></div>
+            <img src={img} alt="camera" />
+          </div>
+          <div className='face back'>
+            <p className='text'>{p.text}</p>
+            <p>From : {p.from}</p>
+            <audio src={p.src} controls></audio>
+          </div>
+        </div>
+      ))}
     </div>
+    </>
   );
 }
 
