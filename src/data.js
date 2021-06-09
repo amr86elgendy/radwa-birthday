26ea379cf4a4a3c80d399a5288b916b4e6de7677
amr elgendy <amr86tawfik@gmail.com>
2021-06-09 21:23:34 +0200

    completed

--- src/data.js
+++ src/data.js
@@ -1,133 +1,114 @@
 const data = [
   {
     id: 1,
-    text: "ana tawfik",
     from: "tawfik",
     audio: "/tawfik.ogg",
     cover: "/cover.png",
   },
   {
     id: 3,
-    text: "ana amany",
     from: "amany",
-    audio: "",
+    audio: "amany.ogg",
     cover: "/cover.png",
   },
   {
     id: 2,
-    text: "ana omniea",
     from: "remonda",
     audio: "remonda.ogg",
     cover: "/cover.png",
   },
   {
     id: 4,
-    text: "ana negm",
     from: "mayar",
     audio: "mayar.ogg",
     cover: "/cover.png",
   },
   {
     id: 5,
-    text: "ana nagy",
     from: "ahmed negm",
     audio: "ahmed-negm.ogg",
     cover: "/cover.png",
   },
   {
     id: 6,
-    text: "ana hady",
     from: "hossam",
     audio: "hossam.ogg",
     cover: "/cover.png",
   },
   {
     id: 7,
-    text: "ana tamer",
     from: "mariam atef",
     audio: "mariam-atef.ogg",
     cover: "/cover.png",
   },
   {
     id: 8,
-    text: "ana amr",
     from: "amal abdallah",
     audio: "amal-abdallah.ogg",
     cover: "/cover.png",
   },
   {
     id: 9,
-    text: "",
     from: "basma",
     audio: "basma.ogg",
     cover: "/cover.png",
   },
   {
     id: 10,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "eman",
     audio: "eman.ogg",
     cover: "/cover.png",
   },
   {
     id: 11,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "maram",
     audio: "maram.ogg",
     cover: "/cover.png",
   },
   {
     id: 12,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "tasneem",
     audio: "tasneem.ogg",
     cover: "/cover.png",
   },
   {
     id: 13,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "yasmina",
     audio: "yasmina.ogg",
     cover: "/cover.png",
   },
   {
     id: 14,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "mariam raouf",
     audio: "mariam-raouf.ogg",
     cover: "/cover.png",
   },
   {
     id: 15,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "mariam raouf",
     audio: "mariam-raouf-2.ogg",
     cover: "/cover.png",
   },
   {
     id: 16,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "heba and her children",
     audio: "heba-we-children-1.ogg",
     cover: "/cover.png",
   },
   {
     id: 17,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "heba and her children",
     audio: "heba-we-children-2.ogg",
     cover: "/cover.png",
   },
   {
     id: 18,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "consaltant",
     audio: "consaltant.ogg",
     cover: "/cover.png",
   },
   {
     id: 19,
-    text: "Happy Birthday Dr/Radwa i hope you the best",
     from: "omnya",
     audio: "omnya.ogg",
     cover: "/cover.png",
@@ -142,12 +123,12 @@ const data = [
     image: '/card-2.jpeg',
     cover: '/cover.png'
   },
-  {
-    id: 22,
-    from: 'guess who ?',
-    audio: 'lobna.ogg',
-    cover: '/cover.png'
-  },
+    {
+      id: 22,
+      from: 'guess who ?',
+      audio: 'lobna.ogg',
+      cover: '/cover.png'
+    },
   {
     id: 23,
     image: '/card-3.jpeg',
@@ -156,6 +137,12 @@ const data = [
   {
     id: 24,
     image: '/card-4.jpeg',
+    cover: '/cover.png'
+  },
+  {
+    id: 25,
+    from: 'heba',
+    audio: 'heba.ogg',
     cover: '/cover.png'
   },
 ];
